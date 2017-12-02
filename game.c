324064e54f7d1bfd242f25fcdfc202848b739165
Unknown <duquenoyantoine.study@gmail.com>
2017-12-02 12:52:02 +0100

    Améliorations jeu
    * Les bordures sont gérées

--- game.c
+++ game.c
@@ -32,25 +32,30 @@ void set_dir(char c)
 
 void move_snake(int dir)
 {
+	int max_y = 0, max_x = 0;
+	getmaxyx(stdscr, max_y, max_x);
 	switch(dir) {
 		case UP:
 			y--;
+			if(y == -1){y=max_y;}
 			break;
 		case DOWN:
 			y++;
+			if(y == max_y+1){y=0;}
 			break;
 		case RIGHT:
 			x++;
+			if(x == max_x+1){x=0;}
 			break;
 		case LEFT:
 			x--;
+			if(x == -1){x=max_x;}
 			break;
 	}
 }
 
 int main(int argc, char *argv[]) {
 
-	int max_y = 0, max_x = 0;
 	int next_x = 0;
 	int next_y = 0;
 	int ch;
@@ -65,8 +70,6 @@ int main(int argc, char *argv[]) {
 		ch = getch();
 		set_dir(ch);
 		move_snake(dir);
-
-		//getmaxyx(stdscr, max_y, max_x);
 		
 		clear();
 		mvprintw(y, x, "o");

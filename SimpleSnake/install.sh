e822810a0b1a849f3b49c052f8d1aa8799855197
AntoineDuq <duquenoyantoine.study@gmail.com>
2017-12-10 17:21:40 +0100

    Snake : amélioration du script de lancement

--- SimpleSnake/install.sh
+++ SimpleSnake/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+clear
 echo ---------------------------------------------
-echo \* SimpleSnake 1.0               
+echo \* SimpleSnake 1.2              
 echo \* by Antoine Duquenoy and Anthony Durot
 echo ---------------------------------------------
 
@@ -9,16 +10,23 @@ isInstalled=`dpkg -s libncurses5 | grep Status | cut -d' ' -f3`
 
 if [ $isInstalled = "ok" ]
 then
+	echo -e "\n"
 	echo "NCURSES 5 is already installed"
-
+	echo -e "\n"
 else
+	echo -e "\n"
 	echo "NCURSES 5 is not installed"
-	sudo apt-get install libncurses5-dev		
+	sudo apt-get install libncurses5-dev
+	echo -e "\n"
 fi
 
 gcc -o game game.c -lncurses
-echo Start the game [y\/n]
-read yn
+echo "-------- INSTRUCTIONS --------"
+echo "* Use the arrow keys to move"
+echo "* ESCAPE to pause"
+echo "------------------------------"
+echo -e "\n"
+read -p "Start the game [y/n] : " yn
 if [ $yn = "y" ] || [ $yn = "Y" ]
 then
 	./game

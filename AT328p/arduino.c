857ed564952ba22082f05ad81c2fece9aa918875
Anthony <gibioctet@gmail.com>
2017-12-11 10:05:35 +0000

    Ajout du code de base AT16U2, finissions AT328P

--- AT328p/arduino.c
+++ AT328p/arduino.c
@@ -27,14 +27,16 @@
 #define BAUDRATE 103
 
 volatile uint8_t ancien_portd = 0xFF; 
+volatile int state = -1;
+volatile int state_led = -1;
 
 void init_boutons(void){    
-   DDRD &= ~((1 << DDD3) | (1 << DDD4) | (1 << DDD5) | (1 << DDD6));
+   DDRD &= ~((1 << DDD2) | (1 << DDD3) | (1 << DDD4) | (1 << DDD5) | (1 << DDD6));
    //PD3,4,5,6 sont maintenant des entrées
-   PORTD |= ((1 << PORTD3) | (1 << PORTD4) | (1 << PORTD5) | (1 << PORTD6));
+    PORTD |= ((1 << PORTD2) | (1 << PORTD3) | (1 << PORTD4) | (1 << PORTD5) | (1 << PORTD6));
    //On active le PULL-UP sur les entrées
    PCICR |= (1 << PCIE2);
-   PCMSK2 |= ((1<< PCINT19) | (1<< PCINT20) | (1<< PCINT21) | (1<< PCINT22));
+PCMSK2 |= ((1 << PCINT18) | (1 << PCINT19) | (1 << PCINT20) | (1 << PCINT21) | (1 << PCINT22));
    sei();
 }
 
@@ -43,9 +45,12 @@ void init_serial(void){
     UBRR0H = 0;
     UBRR0L = baudrate;
 
-    UCSR0B = (1<<TXEN0);
+    UCSR0B = ((1 << RXCIE0) |(1 << TXEN0) | (1 << RXEN0));
 
-    UCSR0C = 0x06;
+    UCSR0C = ((1 << UCSZ01) | (1 << UCSZ00));
+
+    UCSR0A &= ~(1 << U2X0);
+    //UCSR0C = 0x06;
 }
 
 void send_serial(unsigned char c)
@@ -54,61 +59,39 @@ void send_serial(unsigned char c)
     UDR0 = c;
 }
 
-void wait(void){
-    cli();
-    _delay_ms(10);
-    sei();
+void output_init(void){
+	DDRB |= 0x20;
 }
 
+ISR (USART_RX_vect){
+	if(UDR0 == 49){//Allumage de la LED
+		PORTB |= 0x20;
+	}
+	if(UDR0 == 48){//Extinction de la LED
+		PORTB &= 0xDF;
+	}
+}
 
 ISR (PCINT2_vect){
-    uint8_t bits_change;
-    bits_change = PIND ^ ancien_portd; 
-    ancien_portd = PIND;
-    if(bits_change & (1 << PIND3) ){
-        if( (PIND & (1 << PIND3) ) == (1 << PIND3)){
-            //Front montant
-            send_serial('d');
-        }
-        else{
-            send_serial('D');
-            //Front descendant
-        }
-    }
-    if(bits_change & (1 << PIND4) ){
-        if( (PIND & (1 << PIND4) ) == (1 << PIND4)){
-            send_serial('h');
-        }
-        else{
-            send_serial('H');
-        }
-    }
-    if(bits_change & (1 << PIND5) ){
-        if( (PIND & (1 << PIND5) ) == (1 << PIND5)){
-            send_serial('b');
-        }
-        else{
-            send_serial('B');
-        }
-    }
-    if(bits_change & (1 << PIND6) ){
-        if( (PIND & (1 << PIND6) ) == (1 << PIND6)){
-            send_serial('g');
-        }
-        else{
-            send_serial('G');
-        }
-    }
-    /* TODO : Gerer les rebondissements */
-    wait();
+    state = ((~(PIND) & 0x7C)>>2);
 }
-
 
     
 int main(void){
     init_serial();
     init_boutons();
+    output_init();
+    uint8_t msg;
     while(1){
+	    if(state!=-1){
+		    msg = state | 0x20;
+		    if(msg != 0x20)
+			{
+		   		 send_serial(msg);
+				 _delay_ms(100);
+		   		 state = -1;
+			}
+	    }
     }
     return 0;
 }

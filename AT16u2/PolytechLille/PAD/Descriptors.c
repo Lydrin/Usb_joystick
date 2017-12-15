b9dceec04a4d54e307e1402a0244767f5ae5e2a0
AntoineDuq <duquenoyantoine.study@gmail.com>
2017-12-15 23:53:44 +0100

    LUFA : modification mineure

--- AT16u2/PolytechLille/PAD/Descriptors.c
+++ AT16u2/PolytechLille/PAD/Descriptors.c
@@ -83,7 +83,7 @@ const USB_Descriptor_Configuration_t PROGMEM PAD_ConfigurationDescriptor =
 			.ConfigurationNumber    = 1,
 			.ConfigurationStrIndex  = NO_DESCRIPTOR,
 
-			.ConfigAttributes       = USB_CONFIG_ATTR_RESERVED,
+			.ConfigAttributes       = (USB_CONFIG_ATTR_RESERVED | USB_CONFIG_ATTR_SELFPOWERED),
 
 			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(500)
 		},
